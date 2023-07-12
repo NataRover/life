855a49a830d226aee8f53cd0482e030f553aae8c
NataRover <vip.aleksa78@mail.ru>
2023-07-12 15:40:06 +0300

    ----

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,11 @@
 // import './scss/style.scss'
 
+let menu_btn = document.querySelector(".header-menu-btn");
+let drop_menu = document.querySelector(".container-drop-menu");
 
+function open_drop_menu() {
+  console.log("click");
+  drop_menu.style.display = "block";
+}
+
+menu_btn?.addEventListener("click", open_drop_menu);
