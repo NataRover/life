a39d469f4e9431cbc642aa47d48bf00b736de9fe
NataRover <vip.aleksa78@mail.ru>
2023-07-12 21:19:20 +0300

    ---

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,21 @@
 // import './scss/style.scss'
 
-let menu_btn = document.querySelector(".header-menu-btn");
-let drop_menu = document.querySelector(".container-drop-menu");
+const menu_btn = document.querySelector(".header-menu-btn");
+const drop_menu = document.querySelector(".container-drop-menu") as any;
+const container = document.querySelector(".container");
 
-function open_drop_menu() {
+//нажатие на МЕНЮ
+menu_btn?.addEventListener("click", function () {
   console.log("click");
   drop_menu.style.display = "block";
+  noScroll();
+});
+//скролл окна исчезает
+function noScroll() {
+  document.body.style.overflow = "hidden";
 }
 
-menu_btn?.addEventListener("click", open_drop_menu);
+//скролл окна появляется
+function windowScroll() {
+  document.body.style.overflow = "auto";
+}
